521b9cb03b1464fb65d5ce8c29c995002ddf8695
smir-ram <smir-ram@colab.com>
2024-01-31 21:00:44 +0000

    removed all **/.ipynb_checkpoints/

--- ProteinStructurePredictionCNN/models/.ipynb_checkpoints/loss-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import torch.nn as nn
-
-# Define your custom loss functions here if needed
-loss_fn = nn.CrossEntropyLoss()  # For now: Cross-Entropy Loss
--- ProteinStructurePredictionCNN/models/.ipynb_checkpoints/predict-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def predict(model, input_data):
-    # Define your inference code here
-    model.eval()
-    with torch.no_grad():
-        output = model(input_data)
-    return output
